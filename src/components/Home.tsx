21560f4e231f68945e685878aa34279e96b4150f
checkr <ugochiori@gmail.com>
2024-11-04 09:47:57 +0100

    new info

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import "./home.css";
 import { Canvas } from "@react-three/fiber";
-// import { OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import ImagePlane from "./ImagePlane";
 
 const Home: React.FC = () => {
   return (
     <>
       <div className="home-heading">Home</div>
-      <Canvas camera={{position: [0,0,10]}}>
+      {/* <Canvas camera={{position: [0,0,10]}}>
         <ambientLight
         intensity={0.5}
          />
          <pointLight position={[10,10,10]} />
         <ImagePlane />
-      </Canvas>
-       {/* <Canvas>
+      </Canvas> */}
+       <Canvas>
       <ambientLight />
       <ImagePlane />
       <OrbitControls />
-    </Canvas> */}
+    </Canvas>
     </>
   );
 }; 
